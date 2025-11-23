ea5616c06bdf90efa43fd7e563b15db5a052b65a
NandorVas <vsnandi5353@gmail.com>
2025-11-23 23:20:07 +0100

    IAuthService added

---
+++ RoxfortNeptun/Services/IAuthService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoxfortNeptun.Models;
+
+namespace RoxfortNeptun.Services
+{
+    public interface IAuthService
+    {
+        Task<LoginResult> LoginAsync(string neptunKod, string password);
+        void Logout();
+        bool IsAuthenticated { get; }
+        Students CurrentStudent { get; }
+        event EventHandler<AuthenticationStateChangedEventArgs> AuthenticationStateChanged;
+    }
+
+    public class LoginResult
+    { 
+        bool Success { get; set; } 
+        string Message { get; set; }
+        IUser User { get; set; }
+    }
+
+    public class AuthenticationStateChangedEventArgs : EventArgs
+    {
+        public bool IsAuthenticated { get; set; }
+        public IUser User { get; set; }
+    }
+
+}
