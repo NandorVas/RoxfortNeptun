6643d03beeea04cf451779a5df272f57e5cd551b
NandorVas <vsnandi5353@gmail.com>
2025-11-23 22:56:48 +0100

    restart

--- RoxfortNeptun/Models/DbContext.cs
+++ RoxfortNeptun/Models/DbContext.cs
@@ -19,6 +19,8 @@ namespace RoxfortNeptun.Models
             _connection = new SQLiteAsyncConnection(databasePath, flags);
         }
 
+        //asd
+
         public async Task<bool> InitializeAsync()
         {
             try
