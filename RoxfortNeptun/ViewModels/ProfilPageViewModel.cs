a9ebbd518394c4bde5df769ad2d76049b39baac6
NandorVas <vsnandi5353@gmail.com>
2025-12-03 11:59:20 +0100

    Checking Connectivity

--- RoxfortNeptun/ViewModels/ProfilPageViewModel.cs
+++ RoxfortNeptun/ViewModels/ProfilPageViewModel.cs
@@ -10,12 +10,53 @@ using System.Threading.Tasks;
 
 namespace RoxfortNeptun.ViewModels
 {
-    public partial class ProfilPageViewModel : BaseViewModel
+    public partial class ProfilPageViewModel : BaseViewModel, IConnectivity
     {
         public IUser user => CurrentUser;
 
+        public IEnumerable<ConnectionProfile> ConnectionProfiles => throw new NotImplementedException();
+
+        [ObservableProperty]
+        public NetworkAccess networkAccess;
+
+        [ObservableProperty]
+        private string connectionType;
+
         public ProfilPageViewModel(IAuthService auth) : base(auth)
         {
+        }
+
+        public event EventHandler<ConnectivityChangedEventArgs> ConnectivityChanged;
+
+        [RelayCommand]
+        private async Task CheckConnectivity()
+        {
+            NetworkAccess = Connectivity.Current.NetworkAccess;
+            WriteOutConnectivity();
+        }
+
+        private async void WriteOutConnectivity()
+        {
+            if(NetworkAccess == NetworkAccess.None)
+            {
+                ConnectionType = "Nincs internet kapcsolat";
+            }
+            else if(NetworkAccess == NetworkAccess.Unknown)
+            {
+                ConnectionType = "Ismeretlen kapcsolat";
+            }
+            else if(NetworkAccess == NetworkAccess.Local)
+            {
+                ConnectionType = "Helyi hálózati kapcsolat";
+            }
+            else if(NetworkAccess == NetworkAccess.ConstrainedInternet)
+            {
+                ConnectionType = "Korlátozott internet kapcsolat";
+            }
+            else if(NetworkAccess == NetworkAccess.Internet)
+            {
+                ConnectionType = "Internet kapcsolat";
+            }
         }
 
         [RelayCommand]
@@ -24,6 +65,7 @@ namespace RoxfortNeptun.ViewModels
             if (Application.Current is App app)
             {
                 app.SwitchToLogInApp();
+                ConnectionType = string.Empty;
             }
         }
     }
